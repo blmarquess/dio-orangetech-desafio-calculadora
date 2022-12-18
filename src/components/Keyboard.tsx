d4a7d7225eb0098dc5477c2f1a724c34d62e7ae3
Bruno Marques <blmarques.dev@gmail.com>
2022-12-18 18:29:49 -0300

    feat(components): create styled to block keyboard calculator

---
+++ src/components/Keyboard.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const KeyboardDisplay = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 1rem;
+  box-sizing: border-box;
+  background-color: #e41919;
+`;
